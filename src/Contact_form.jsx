18be1fc2810ba450ed535b8579c59ea04ad05f45
DSATISH327 <sathishdesaboina42@gmail.com>
2025-04-04 01:16:00 +0530

    update the contact form

--- src/Contact_form.jsx
+++ src/Contact_form.jsx
@@ -9,25 +9,25 @@ const Contact_form = () => {
         <main className='names'>
             <label htmlFor="First_name">First Name</label>
             <br />
-            <input type="text" name="First_name" id="First_Name" />
+            <input type="text" name="First_name" id="First_Name" required/>
             <br />
             <label htmlFor="Last_name">Last Name</label>
             <br />
-            <input type="text" name="Last_name" id="Last_name" />
+            <input type="text" name="Last_name" id="Last_name" required/>
         </main>
         <main className='mail_phone_no'>
-            <label htmlFor="Email">Email</label>
+            <label htmlFor="email">Email</label>
             <br />
-            <input type="email" name="Email" id="Email" />
+            <input type="email" name="email" id="email" required />
             <br />
             <label htmlFor="Phone_number">Phone Number</label>
             <br />
-            <input type="tel" name="Phone_number" id="Phone_number" />
+            <input type="tel" name="Phone_number" id="Phone_number" required/>
         </main>
         <main className='Address'>
             <label htmlFor="Address">Address</label>
             <br />
-            <textarea name="Address" id="Address" rows="5" cols="40" placeholder="Type your Address Here..."></textarea>
+            <textarea name="Address" id="Address" rows="5" cols="40" placeholder="Type your Address Here..." required></textarea>
         </main>
         <main className='Submit'>
             <button type="submit" aria-label="Submit the form">SUBMIT</button>
